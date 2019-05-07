6534c24e2326b826482185cd82e9f5a8b9198dc9
John-smith-889 <bartlomiejczajewski@gmail.com>
2019-05-08 01:23:08 +0200

    correlation matrix added

--- feature-engineering.R
+++ feature-engineering.R
@@ -23,5 +23,21 @@ dane <- read.csv2("final_data.csv", header = T, sep = ",", dec = ".")
 # str(dane) # 100k obs
 
 
+######################
+# Correlation Matrix #
+######################
+
+# install.packages("corrplot")
+library(corrplot)
+
+# str(dane)
+
+# Create data matrix from df
+mat <- data.matrix(dane)
+
+# corelation matrix computation
+mydata.cor = cor(mat)
+corrplot(mydata.cor, tl.cex=0.1)
+
 
 
