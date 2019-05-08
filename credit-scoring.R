49a39aa83e79411af4df77d4175c92c34e71dfb6
John-smith-889 <bartlomiejczajewski@gmail.com>
2019-05-08 02:59:29 +0200

    Custom callback function

--- credit-scoring.R
+++ credit-scoring.R
@@ -1,1 +1,49 @@
-#
+
+################################################################################
+# mxnetR - Custom model #       
+#!#######################
+
+# Step by step neural network creation
+
+library(mxnet)
+
+#####################
+# Callback function #
+#####################
+
+# custom callback funtion for making model checkpoints and error log saving 
+# simultaneously
+mx.callback.cus <- function(prefix, period, logger=NULL) {
+  # code for log saving
+  function(iteration, nbatch, env, verbose) {
+    if (nbatch %% period == 0 && !is.null(env$metric)) {
+      result <- env$metric$get(env$train.metric)
+      if (nbatch != 0)
+        if(verbose) cat(paste0("Batch [", nbatch, "] Train-", result$name, 
+                               "=", result$value, "\n"))
+      if (!is.null(logger)) {
+        if (class(logger) != "mx.metric.logger") {
+          stop("Invalid mx.metric.logger.")
+        }
+        logger$train <- c(logger$train, result$value)
+        if (!is.null(env$eval.metric)) {
+          result <- env$metric$get(env$eval.metric)
+          if (nbatch != 0)
+            cat(paste0("Batch [", nbatch, "] Validation-", result$name, 
+                       "=", result$value, "\n"))
+          logger$eval <- c(logger$eval, result$value)
+        }
+      }
+    }
+    # code for saving model - checkpoint 
+    # https://mxnet.incubator.apache.org/versions/master/tutorials/r/CallbackFunction.html
+    if (iteration %% period == 0) {
+      mx.model.save(env$model, prefix, iteration)
+      if(verbose) cat(sprintf("Model checkpoint saved to %s-%04d.params\n", 
+                              prefix, iteration))
+    }
+    return(TRUE)
+  }
+}
+
+
